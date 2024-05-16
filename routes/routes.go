7fcd0f5c246c5895dc17479955be6cc4b2044089
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-16 16:11:52 +0700

    initial routes

---
+++ routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"mods/controller"
+	"mods/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Routes(router *gin.Engine, carController controller.UserController, jwtService service.JWTService) {
+	userPublic := router.Group("/user")
+	{
+		// public can access
+		userPublic.POST("/add", carController.AddUser)
+		userPublic.GET("", carController.GetAllUser)
+
+	}
+
+}
