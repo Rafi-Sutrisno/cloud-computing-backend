ec75a19e5697cb8986c31193c459d5b1cb603e61
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-16 16:10:28 +0700

    initial controller

---
+++ controller/user-controller.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	// "encoding/json"
+	"mods/dto"
+	"mods/service"
+	"mods/utils"
+	"net/http"
+
+	// "os/exec"
+	// "strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userController struct {
+	userService service.UserService
+	jwtService  service.JWTService
+}
+
+type UserController interface {
+	// regist login
+	AddUser(ctx *gin.Context)
+	GetAllUser(ctx *gin.Context)
+}
+
+func NewUserController(us service.UserService, jwt service.JWTService) UserController {
+	return &userController{
+		userService: us,
+		jwtService:  jwt,
+	}
+}
+
+func (uc *userController) AddUser(ctx *gin.Context) {
+
+	var user dto.CreateUserDTO
+	if tx := ctx.ShouldBind(&user); tx != nil {
+		res := utils.BuildErrorResponse("Failed to process request", http.StatusBadRequest)
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	result, err := uc.userService.CreateUser(ctx.Request.Context(), user)
+	if err != nil {
+		res := utils.BuildErrorResponse("Failed to register user", http.StatusBadRequest)
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	res := utils.BuildResponse("Success to register user", http.StatusOK, result)
+	ctx.JSON(http.StatusOK, res)
+}
+
+func (uc *userController) GetAllUser(ctx *gin.Context) {
+	userList, err := uc.userService.GetAllUser(ctx)
+	if err != nil {
+		res := utils.BuildErrorResponse(err.Error(), http.StatusBadRequest)
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	res := utils.BuildResponse("success ini mobil mu", http.StatusOK, userList)
+	_ = res
+	ctx.JSON(http.StatusOK, res)
+}
