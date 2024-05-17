3c859d68c8e50ce62736bc4193aee79e746e628c
Ahdaaa <afilzag@gmail.com>
2024-05-17 21:37:56 +0700

    feat: new entity prediction

--- entity/prediction.go
+++ entity/prediction.go
@@ -1,10 +1,12 @@
 package entity
 
+import "time"
+
 type Prediction struct {
-	ID             uint64 `json:"id" gorm:"primaryKey"`
-	Gambar         string `json:"gambar" binding:"required"`
-	Hasil_Prediksi string `json:"hasil_prediksi" binding:"required"`
-	Tgl            string `json:"tgl" binding:"required"`
-	UserID         uint64 `gorm:"foreignKey" json:"user_id"`
-	User           *User  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user,omitempty"`
+	Pr_ID          string    `json:"id" gorm:"primaryKey"`
+	Gambar         string    `json:"gambar" binding:"required"`
+	Hasil_Prediksi string    `json:"hasil_prediksi" binding:"required"`
+	Tgl            time.Time `json:"tgl" binding:"required"`
+	UserID         string    `gorm:"foreignKey" json:"user_id"`
+	User           *User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user,omitempty"`
 }
